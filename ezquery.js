8a391cb13f2d7eab45fd945dffc3d6f964826001
Robert Honsby <rhonsby@gmail.com>
2016-03-05 13:56:40 -0800

    Adds basic core functionality

--- ezquery.js
+++ ezquery.js
@@ -1,1 +1,12 @@
-console.log('script loaded!');
+(function(root) {
+  root.$ez = function(selector) {
+    var nodeList = document.querySelectorAll(selector);
+    var elements = Array.prototype.slice.call(nodeList);
+
+    return new DOMNodeCollection(elements);
+  };
+
+  function DOMNodeCollection(elements) {
+    this.elements = elements;
+  }
+})(this);
